569c77ef744a326d233ec39290c3ebd165671f2a
Quintin Smith <quintin@getweave.com>
2015-11-20 14:13:08 -0700

    UIViews and drawing on them using Bezier Paths

---
+++ Stack/FaceViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FaceViewController.swift
+//  Stack
+//
+//  Created by Quintin Smith on 11/20/15.
+//  Copyright © 2015 wasatch code. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+    
+    
+
+}
--- Stack/ViewController.swift
+++ Stack/ViewController.swift
@@ -8,18 +8,67 @@
 
 import UIKit
 
+class ShapeView: UIView {
+    let size: CGFloat = 100.00
+    
+    init(origin: CGPoint) {
+        super.init(frame: CGRectMake(0.0 , 0.0, size, size))
+        self.center = origin
+        self.backgroundColor = UIColor.clearColor()
+    }
+    
+    //We need to implement init(coder) to avoid compilation errors
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func drawRect(rect: CGRect) {
+        let path = UIBezierPath(roundedRect: rect, cornerRadius: 10)
+        
+        UIColor.grayColor().setFill()
+        path.fill()
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        let tapGR = UITapGestureRecognizer(target: self, action: "didTap:")
+        self.view.addGestureRecognizer(tapGR)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//        let path = UIBezierPath()
+//        
+//        path.moveToPoint(CGPoint(x: 80, y: 50))
+//        path.addLineToPoint(CGPoint(x: 140, y:150))
+//        path.addLineToPoint(CGPoint(x: 10, y:150))
+//        
+//        path.closePath()
+//        
+//        UIColor.greenColor().setFill()
+//        UIColor.redColor().setStroke()
+//        path.lineWidth = 3.0
+//        path.fill()
+//        path.stroke()
+        
     }
-
+    
+    func didTap(tapGR: UITapGestureRecognizer) {
+        
+       let tapPoint = tapGR.locationInView(self.view)
+        
+        let shapeView = ShapeView(origin: tapPoint)
+        
+        self.view.addSubview(shapeView)
+        
+        let label = UILabel(frame: CGRectMake(0, 0, 50, 50))
+        label.center = shapeView.center
+        label.textAlignment = NSTextAlignment.Center
+        label.textColor = UIColor.whiteColor()
+        label.text = "Jimmy"
+        
+        self.view.addSubview(label)
+    }
 
 }
 
